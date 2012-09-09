a5771dd3d5a33525cd8e35caa6af584141759c9e
Michael Weiss <mweiss@ixl.com>
2012-09-08 23:35:44 -0700

    commit

---
+++ StepSynth/CircleSelectView.h
@@ -0,0 +1,13 @@
+//
+//  CircleSelectView.h
+//  StepSynth
+//
+//  Created by Michael Weiss on 9/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CircleSelectView : UIView
+
+@end
---
+++ StepSynth/CircleSelectView.m
@@ -0,0 +1,31 @@
+//
+//  CircleSelectView.m
+//  StepSynth
+//
+//  Created by Michael Weiss on 9/8/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "CircleSelectView.h"
+
+@implementation CircleSelectView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
